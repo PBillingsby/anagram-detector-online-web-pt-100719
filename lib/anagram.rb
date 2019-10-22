48a79f0b44cd9a0d5bf2206a1933b0d98b8aef03
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-22 19:26:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  @@words = []
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(@word) 
+    if @word.split("").sort == @word.split("").sort
+      @@words << @word
+    end
+    @@words
+  end
+end
